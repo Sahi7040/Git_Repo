660aabb9519b57d194b41b7f8235d7705d3a257e
saurabh <Saurabh@DESKTOP-JV58V6R>
2023-03-10 06:31:26 +0530

    this is my first commit file

---
+++ gitdemoprojectv1/src/gitdemoprojectv1/Test.java
@@ -0,0 +1,20 @@
+package gitdemoprojectv1;
+
+
+public class Test {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String hungry= "No";
+		
+		if(hungry== "yes") {
+			System.out.println("eat something ......");		
+			}
+		else {
+			System.out.println("do work....");
+		}
+	}
+
+}
